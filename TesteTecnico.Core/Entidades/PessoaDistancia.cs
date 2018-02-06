e51914a651b70f6752ed100bed83e9d7fea0e86c
seankalano <seankalano@hotmail.com>
2018-02-06 00:46:38 -0200

    Ajuste de Using

--- TesteTecnico.Core/Entidades/PessoaDistancia.cs
+++ TesteTecnico.Core/Entidades/PessoaDistancia.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TesteTecnico.Core.Entidades
+﻿namespace TesteTecnico.Core.Entidades
 {
     public class PessoaDistancia : Pessoa
     {
